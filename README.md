371eeb52b5da5a3d5221ff56d59565e1f402d628
sahil khatri <sahilkeshav1997@gmail.com>
2024-06-18 11:01:51 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+- react image carousel
+- change slide on click of prev and next buttons (check for edge cases)
+- css important to understand
+- react icons
+- can also go for react slick library
---
+++ src/Carousel.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import { FaQuoteRight } from 'react-icons/fa'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import { longList } from './data'
+
+const Carousel = () => {
+  const [people, setPeople] = useState(longList)
+  const [currentPerson, setCurrentPerson] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide()
+    }, 2000)
+
+    return () => clearInterval(interval)
+  }, [currentPerson])
+
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length
+      return result
+    })
+  }
+
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length
+      return result
+    })
+  }
+
+  return (
+    <section className='slider-container'>
+      {people.map((person, personIndex) => {
+        const { id, image, name, title, quote } = person
+
+        return (
+          <article
+            key={id}
+            className='slide'
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? 1 : 0,
+              visibility: personIndex === currentPerson ? 'visible' : 'hidden',
+            }}
+          >
+            <img src={image} alt={name} className='person-img' />
+            <h5 className='name'>{name}</h5>
+            <p className='title'>{title}</p>
+            <p className='text'>{quote}</p>
+            <FaQuoteRight className='icon' />
+          </article>
+        )
+      })}
+      <button className='prev' onClick={prevSlide} type='button'>
+        <FiChevronLeft />
+      </button>
+      <button className='next' onClick={nextSlide} type='button'>
+        <FiChevronRight />
+      </button>
+    </section>
+  )
+}
+
+export default Carousel
